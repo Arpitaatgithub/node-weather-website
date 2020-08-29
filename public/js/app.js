20f1ccf5f547515cf661124c0b76166950b58a79
Arpita Yadav <ArpitaY@hcl.com>
2020-08-29 22:09:05 +0530

    prod setup

--- public/js/app.js
+++ public/js/app.js
@@ -21,7 +21,7 @@ weatherForm.addEventListener('submit', (e)=>{
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
 
-    fetch('http://localhost:3000/weather?address=' + coords).then((response) => {
+    fetch('/weather?address=' + coords).then((response) => {
     response.json().then((data) => {
         if(data.error){
                 console.log(data.error);
